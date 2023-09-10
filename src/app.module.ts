793f2e579de90561072c9345a55b3614c985c50a
Roihan <hroihan6@gmail.com>
2023-09-10 15:46:28 +0700

    Connect to DB using typeorm & set session configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot()],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: (process.env.DB_TYPE as any) || 'mysql',
+      host: process.env.DB_HOST,
+      port: (process.env.DB_PORT as any) || 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User],
+      synchronize: true,
+    }),
+    PassportModule.register({ session: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
